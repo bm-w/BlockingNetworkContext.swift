d2fba5093823fe7fc96f8b1c5fd37dd40d9fe37f
Bastiaan Marinus van de Weerd <bastiaan+git@bm-w.eu>
2017-07-13 08:46:57 -0400

    Initial.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Blocking networking
+
+```swift
+// ...
+```
---
+++ main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  BlockingNetwork
+//
+//  Created by Bastiaan Marinus van de Weerd on 11/07/2017.
+//
+//
+
+
+import Foundation
+
+
+print("Go...")
+sleep(1)
+print("Ding!")
